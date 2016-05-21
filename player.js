9c8d8fb39195b5541daa3305864d5d33088b6d12
fabian-froehlich <froehlich.fabian@t-online.de>
2016-05-21 18:30:45 +0200

    yolo

--- player.js
+++ player.js
@@ -4,7 +4,13 @@ module.exports = {
   VERSION: "Kiel #1 Team",
 
   bet_request: function(game_state, bet) {
-    //bet(1000);
+    var small = game_state.small_blind;
+    var we;
+    gemes_state.players.forEach(function(player){
+      if (player.name == 'All in'){
+        we = player;
+      }
+    });
     bet(Math.floor((Math.random() * 1000) + 300));
       
   },
