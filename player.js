b4f9ef83ed99ee63c442a768339076327d36c34a
fabian-froehlich <froehlich.fabian@t-online.de>
2016-05-21 18:40:28 +0200

    beautify

--- player.js
+++ player.js
@@ -1,22 +1,22 @@
-
 module.exports = {
 
-  VERSION: "Kiel #1 Team",
+    VERSION: "Kiel #1 Team",
 
-  bet_request: function(game_state, bet) {
-    var small = game_state.small_blind;
-    var we;
-    game_state.players.forEach(function(player){
-        if (player.name == 'All In'){
-            we = player;
-            console.log(we);   
-      }
-    });
-    bet(Math.floor((Math.random() * 1000) + 300));
-      
-  },
+    bet_request: function (game_state, bet) {
+        var small = game_state.small_blind;
+        var we;
+        game_state.players.forEach(function (player) {
+            if (player.name == 'All In') {
+                we = player;
+                console.log(we);
+            }
+        });
 
-  showdown: function(game_state) {
+        bet(Math.floor((Math.random() * 1000) + 300));
 
-  }
+    },
+
+    showdown: function (game_state) {
+
+    }
 };
