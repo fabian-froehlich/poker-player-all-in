d1162971b69d374ff5bb9cae5fbd474bc9617782
fabian-froehlich <froehlich.fabian@t-online.de>
2016-05-21 19:52:35 +0200

    yolo

--- player.js
+++ player.js
@@ -3,34 +3,40 @@ module.exports = {
     VERSION: "Kiel #1 Team",
 
     bet_request: function (game_state, bet) {
-        var sb = game_state.small_blind;
-        var orbits = game_state.orbits;
-        var cc = game_state.community_cards;
-        var cb = game_state.current_buy_in;
-        var pot = game_state.pot;
-        var we;
-        
-        game_state.players.forEach(function (player) {
-            if (player.name == 'All In') {
-                we = player;
-                console.log("Printing Player")
-                console.log(we);
-            }
-        });
-        var hc = we.hole_cards;
-        console.log("Printing Whole Cards:");
-        console.log(hc);
-        console.log("Printing All Community Cards");
-        console.log(cc);
-        console.log("Printing CC in loop")
-        cc.forEach(function (card) {
-            console.log("Cards");
-            console.log(card);
-        });
-        
+        //max and min for random bet
         var max = 1000;
         var min = 300;
-        bet(Math.floor(Math.random() * (max - min + 1) + min));
+        try {
+            var sb = game_state.small_blind;
+            var orbits = game_state.orbits;
+            var cc = game_state.community_cards;
+            var cb = game_state.current_buy_in;
+            var pot = game_state.pot;
+            var we;
+
+            game_state.players.forEach(function (player) {
+                if (player.name == 'All In') {
+                    we = player;
+                    console.log("Printing Player")
+                    console.log(we);
+                }
+            });
+            var hc = we.hole_cards;
+            console.log("Printing Whole Cards:");
+            console.log(hc);
+            console.log("Printing All Community Cards");
+            console.log(cc);
+            console.log("Printing CC in loop")
+            cc.forEach(function (card) {
+                console.log("Cards");
+                console.log(card);
+            });
+
+            bet(Math.floor(Math.random() * (max - min + 1) + min));
+        } catch (e) {
+            bet(Math.floor(Math.random() * (max - min + 1) + min));
+
+        }
 
     },
 
