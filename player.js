26d97c03a54bb61440e57dc3444f93ad4a0209e7
fabian-froehlich <froehlich.fabian@t-online.de>
2016-05-21 18:21:42 +0200

    yolo

--- player.js
+++ player.js
@@ -5,7 +5,7 @@ module.exports = {
 
   bet_request: function(game_state, bet) {
     //bet(1000);
-    Math.floor((Math.random() * 100) + 300);
+    bet(Math.floor((Math.random() * 1000) + 300));
       
   },
 
